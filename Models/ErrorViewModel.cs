86d0067c11d16c6cdabb36ae6b266ef5189dda3a
victor1127 <95936106+victor1127@users.noreply.github.com>
2022-02-17 12:01:42 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FizzBuzzProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
